b3733aa3eaa772282a64672a420adcf2246c4989
ParnThanatibordee <parn8946@gmail.com>
2023-04-23 15:04:51 +0700

    change require to import

--- vote/src/index.js
+++ vote/src/index.js
@@ -1,8 +1,7 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 import dotenv from 'dotenv'
-
-const cors = require('cors')
+import cors from 'cors'
 
 import vote from './routes/vote.route.js'
 
